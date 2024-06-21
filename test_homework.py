98b8335cbb0d6db62f66c7acdac8bc32cb749c9a
Timur <Stanmr7n@gmail.com>
2024-06-21 15:56:25 +0700

    added a file requirements.txt removed prints from test_homework.py

--- test_homework.py
+++ test_homework.py
@@ -1,33 +1,32 @@
 import math
 import random
-
-
 def test_greeting():
+
     name = "Анна"
     age = 25
 
     output = f"Привет, {name}! Тебе {age} лет."
-
+    print(output)
 
     assert output == "Привет, Анна! Тебе 25 лет."
 
-
 def test_rectangle():
+
     a = 10
     b = 20
 
-    perimeter = (a + b) * 2
+    perimeter = (a + b)*2
 
 
     assert perimeter == 60
 
-    area = a * b
+    area = a*b
 
 
     assert area == 200
 
-
 def test_circle():
+
     PI = math.pi
     r = 23
 
@@ -38,7 +37,8 @@ def test_circle():
     length = 2 * PI * r
 
     assert length == 144.51326206513048
-
+    print("Площадь=", area)
+    print("Длина=", length)
 
 def test_random_list():
     l = [random.randint(1, 100) for _ in range(10)]
@@ -48,18 +48,19 @@ def test_random_list():
     assert len(l) == 10
     assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
 
-
 def test_unique_elements():
+
     l = {1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10}
 
     l = list(set(l))
+
 
     assert isinstance(l, list)
     assert len(l) == 10
     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-
 def test_dicts():
+
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
 
