3b9749d750fbea277eb599123732e49cff7f459b
Tim Latypov <96415981+stannymrtn@users.noreply.github.com>
2024-06-21 12:52:19 +0700

    Delete test_random_list.py

--- test_random_list.py
+++
@@ -1,9 +0,0 @@
-import random
-def test_random_list():
- 
-    l = [random.randint(1,100) for _ in range(10)]
-    l.sort()
-    print(l)
-
-    assert len(l) == 10
-    assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
